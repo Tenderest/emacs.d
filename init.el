24f32054eea4622773b6e1dc9e9e964c25ed1d36
Tenderest <2087886585@qq.com>
2020-04-04 21:33:51 +0800

    2020/04/04 :pencil2: 更新

--- init.el
+++ init.el
@@ -2,8 +2,89 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+ (when (>= emacs-major-version 24)
+     (require 'package)
+     (package-initialize)
+     (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+		      ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
 
+;; 注意 elpa.emacs-china.org 是 Emacs China 中文社区在国内搭建的一个 ELPA 镜像
+
+ ;; cl - Common Lisp Extension
+ (require 'cl)
+
+ ;; Add Packages
+ (defvar my/packages '(
+		;; --- Auto-completion ---
+		company
+		;; --- Better Editor ---
+		hungry-delete
+		swiper
+		counsel
+		smartparens
+		js2-mode
+		;; --- Themes ---
+		monokai-theme
+		;; solarized-theme
+		) "Default packages")
+
+ (setq package-selected-packages my/packages)
+
+ (defun my/packages-installed-p ()
+     (loop for pkg in my/packages
+	   when (not (package-installed-p pkg)) do (return nil)
+	   finally (return t)))
+
+ (unless (my/packages-installed-p)
+     (message "%s" "Refreshing package database...")
+     (package-refresh-contents)
+     (dolist (pkg my/packages)
+       (when (not (package-installed-p pkg))
+	 (package-install pkg))))
+
+;; ================插件的配置=========================
+;; 启动石加载主题
+(load-theme 'monokai 1)
+
+
+;; js2-mode configurations 设置打开JavaScript文件的Major Mode
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
+
+
+;; hungry-delete configurations 快速删除多个空格
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+
+;; Swiper&counsel configurations 更好的提示
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+;; enable this if you want `swiper' to use it
+;; (setq search-default-mode #'char-fold-to-regexp)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+
+
+;; smartparens configurations 自动补全括号
+(require 'smartparens-config)
+;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+(smartparens-global-mode t)
+
+;; =========================================
+
+
+
+
+;; ===========子龙山人的配置======================
 ;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
 ;;(tool-bar-mode -1)
 
@@ -20,7 +101,6 @@
 (defun open-init-file ()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
-
 ;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
 (global-set-key (kbd "<f12>") 'open-init-file)
 
@@ -31,16 +111,45 @@
 (setq make-backup-files nil)
 
 
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-item 20)
+
+;; 这个快捷键绑定可以用之后的插件 counsel 代替
+(global-set-key (kbd "C-x C-r") 'recentf-open-files)
+
+;; 替换选中部分文字
+(delete-selection-mode 1)
+
+;; 将 Emacs 设置为开启默认全屏
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+;; 开启括号匹配
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+;; 高亮当前行，当文本内容很多时可以很容易找到光标的位置
+(global-hl-line-mode 1)
+
+;; 设置默认 Org Agenda 文件目录
+(setq org-agenda-files '("~/Org_files"))
+
+;; 设置 org-agenda 打开快捷键
+(global-set-key (kbd "C-c a") 'org-agenda)
+;; ===========子龙山人的配置======================
+
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-idle-delay 0.05)
+ '(company-minimum-prefix-length 1)
  '(package-selected-packages (quote (company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(js2-external-variable ((t (:foreground "dim gray")))))
